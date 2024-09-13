ffcda2c84a3e2ad83805cda55c8fa2bf67cad133
Anne Apiyo <anneapiyo0@gmail.com>
2024-09-13 17:29:00 +0300

    project created

--- msm_management_app/lib/screens/income_expense_screen.dart
+++ msm_management_app/lib/screens/income_expense_screen.dart
@@ -7,6 +7,8 @@ class IncomeExpenseScreen extends StatefulWidget {
 
 class _IncomeExpenseScreenState extends State<IncomeExpenseScreen> {
   final List<Map<String, dynamic>> _transactions = [];
+  double totalIncome = 0.0;
+  double totalExpense = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -16,30 +18,48 @@ class _IncomeExpenseScreenState extends State<IncomeExpenseScreen> {
       ),
       body: Column(
         children: [
+          // Display total balance at the top
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Total Income: KES $totalIncome',
+                    style: TextStyle(fontSize: 18)),
+                Text('Total Expense: KES $totalExpense',
+                    style: TextStyle(fontSize: 18)),
+              ],
+            ),
+          ),
+
           Expanded(
             child: ListView.builder(
               itemCount: _transactions.length,
               itemBuilder: (context, index) {
                 return ListTile(
                   title: Text(
-                      '${_transactions[index]["type"]}: ${_transactions[index]["amount"]}'),
+                      '${_transactions[index]["type"]}: KES ${_transactions[index]["amount"]}'),
                 );
               },
             ),
           ),
+
           Row(
             children: [
               Expanded(
                 child: TextField(
                   onSubmitted: (value) {
                     setState(() {
-                      _transactions.add({"type": "Income", "amount": value});
+                      double income = double.parse(value);
+                      _transactions.add({"type": "Income", "amount": income});
+                      totalIncome += income;
                     });
                   },
                   decoration: InputDecoration(
                     labelText: 'Add Income',
                     border: OutlineInputBorder(),
                   ),
+                  keyboardType: TextInputType.number,
                 ),
               ),
               SizedBox(width: 8),
@@ -47,13 +67,16 @@ class _IncomeExpenseScreenState extends State<IncomeExpenseScreen> {
                 child: TextField(
                   onSubmitted: (value) {
                     setState(() {
-                      _transactions.add({"type": "Expense", "amount": value});
+                      double expense = double.parse(value);
+                      _transactions.add({"type": "Expense", "amount": expense});
+                      totalExpense += expense;
                     });
                   },
                   decoration: InputDecoration(
                     labelText: 'Add Expense',
                     border: OutlineInputBorder(),
                   ),
+                  keyboardType: TextInputType.number,
                 ),
               ),
             ],
